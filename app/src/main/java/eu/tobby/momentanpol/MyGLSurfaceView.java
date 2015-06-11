95b1aa286f38c377c943944b18cf24eaac74f3d9
Tobias Holst <tobby@tobby.eu>
2015-06-11 19:36:25 +0200

    add OpenGL Renderer

---
+++ app/src/main/java/eu/tobby/momentanpol/MyGLSurfaceView.java
@@ -0,0 +1,24 @@
+package eu.tobby.momentanpol;
+
+import android.opengl.GLSurfaceView;
+import android.content.Context;
+
+/**
+ * Created by tobby on 11.06.15.
+ */
+class MyGLSurfaceView extends GLSurfaceView {
+
+    private final MyGLRenderer mRenderer;
+
+    public MyGLSurfaceView(Context context){
+        super(context);
+
+        // Create an OpenGL ES 2.0 context
+        setEGLContextClientVersion(2);
+
+        mRenderer = new MyGLRenderer();
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        setRenderer(mRenderer);
+    }
+}
