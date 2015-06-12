fcabceed9e1b0bc4c638598dd9059c837241173b
Tobias Holst <tobby@tobby.eu>
2015-06-12 16:54:06 +0200

    remove unnecessary imports

--- app/src/main/java/eu/tobby/momentanpol/OpenGL_Renderer.java
+++ app/src/main/java/eu/tobby/momentanpol/OpenGL_Renderer.java
@@ -8,9 +8,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 
 public class OpenGL_Renderer extends ActionBarActivity {
