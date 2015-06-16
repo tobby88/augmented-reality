12754cf1464428a38f55feb7f495e85574bb4e1f
Tobias Holst <tobby@tobby.eu>
2015-06-16 15:53:23 +0200

    Revert "added an triangle, which can be turned by a touch event"
    This reverts commit 96b004d0d736172177cd1bf61a9214dab7d4f55e.

--- app/src/main/java/eu/tobby/momentanpol/MyGLSurfaceView.java
+++ app/src/main/java/eu/tobby/momentanpol/MyGLSurfaceView.java
@@ -2,7 +2,6 @@ package eu.tobby.momentanpol;
 
 import android.opengl.GLSurfaceView;
 import android.content.Context;
-import android.view.MotionEvent;
 
 /**
  * Created by tobby on 11.06.15.
@@ -11,57 +10,15 @@ class MyGLSurfaceView extends GLSurfaceView {
 
     private final MyGLRenderer mRenderer;
 
-    private final float TOUCH_SCALE_FACTOR = -180.0f / 320;
-    private float mPreviousX;
-    private float mPreviousY;
-
     public MyGLSurfaceView(Context context){
         super(context);
 
         // Create an OpenGL ES 2.0 context
         setEGLContextClientVersion(2);
-        //setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-
 
         mRenderer = new MyGLRenderer();
 
         // Set the Renderer for drawing on the GLSurfaceView
         setRenderer(mRenderer);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent e) {
-        // MotionEvent reports input details from the touch screen
-        // and other input controls. In this case, you are only
-        // interested in events where the touch position changed.
-
-        float x = e.getX();
-        float y = e.getY();
-
-        switch (e.getAction()) {
-            case MotionEvent.ACTION_MOVE:
-
-                float dx = x - mPreviousX;
-                float dy = y - mPreviousY;
-
-                // reverse direction of rotation above the mid-line
-                if (y > getHeight() / 2) {
-                    dx = dx * -1 ;
-                }
-
-                // reverse direction of rotation to left of the mid-line
-                if (x < getWidth() / 2) {
-                    dy = dy * -1 ;
-                }
-
-                mRenderer.setAngle(
-                        mRenderer.getAngle() +
-                                ((dx + dy) * TOUCH_SCALE_FACTOR));
-                requestRender();
-        }
-
-        mPreviousX = x;
-        mPreviousY = y;
-        return true;
     }
 }
--- app/src/main/java/eu/tobby/momentanpol/OpenGL_Renderer.java
+++ app/src/main/java/eu/tobby/momentanpol/OpenGL_Renderer.java
@@ -17,7 +17,6 @@ public class OpenGL_Renderer extends ActionBarActivity {
     private RelativeLayout rl;
     private View androidView;
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
