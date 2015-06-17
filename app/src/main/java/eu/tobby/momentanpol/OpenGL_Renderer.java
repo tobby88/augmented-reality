623664985fb28cb29210ff9e1cc3748ca4404854
Tobias Holst <tobby@tobby.eu>
2015-06-17 21:51:23 +0200

    Show camera image by the use of Vuforia (based on the SampleApp). But camera image still isn't fullscreen.

--- app/src/main/java/eu/tobby/momentanpol/OpenGL_Renderer.java
+++ app/src/main/java/eu/tobby/momentanpol/OpenGL_Renderer.java
@@ -2,16 +2,13 @@ package eu.tobby.momentanpol;
 
 import android.content.Context;
 import android.content.pm.ActivityInfo;
-import android.graphics.Color;
-import android.opengl.GLSurfaceView;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.view.ViewGroup.LayoutParams;
 
 import com.qualcomm.vuforia.CameraDevice;
 import com.qualcomm.vuforia.State;
@@ -19,36 +16,35 @@ import com.qualcomm.vuforia.Vuforia;
 
 
 public class OpenGL_Renderer extends ActionBarActivity implements SampleApplicationControl {
-
-    private GLSurfaceView glView;
-    private RelativeLayout rl;
+    //private GLSurfaceView glView;
     private View androidView;
+
     SampleApplicationSession vuforiaAppSession;
 
-    // Our OpenGL view:
     private SampleApplicationGLView mGlView;
-    // Our renderer;
     private FrameMarkerRenderer mRenderer;
-    private RelativeLayout mUILayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Create a new relative layout to make a stack of views
-        rl = new RelativeLayout(this);
-        // Create object of an OpenGL-Viewer and add this view to the layout
-        glView = new MyGLSurfaceView(this);
-        //rl.addView(glView);
+        vuforiaAppSession = new SampleApplicationSession(this);
+        vuforiaAppSession.initAR(this, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Create object of an OpenGL-Viewer and show this view
+        //glView = new MyGLSurfaceView(this);
+        int depthSize = 16;
+        int stencilSize = 0;
+        boolean translucent = Vuforia.requiresAlpha();
+        mGlView = new SampleApplicationGLView(this);
+        mGlView.init(translucent, depthSize, stencilSize);
+        mRenderer = new FrameMarkerRenderer(this, vuforiaAppSession);
+        mGlView.setRenderer(mRenderer);
+        setContentView(mGlView);
         // Make a view out of the Designer-XML and add this view on top of the OpenGL-Viewer
         LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         androidView = inflater.inflate(R.layout.activity_open_gl__renderer, null);
-        rl.addView(androidView);
-        startLoadingAnimation();
-        vuforiaAppSession = new SampleApplicationSession(this);
-        vuforiaAppSession.initAR(this, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        // Show this layout
-        setContentView(rl);
+        addContentView(androidView, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
     }
+
 
     @Override
     public void onDestroy() {
@@ -89,7 +85,7 @@ public class OpenGL_Renderer extends ActionBarActivity implements SampleApplicat
 
     @Override
     public boolean doLoadTrackersData() {
-        return false;
+        return true;
     }
 
     @Override
@@ -112,45 +108,10 @@ public class OpenGL_Renderer extends ActionBarActivity implements SampleApplicat
         return false;
     }
 
-    private void initApplicationAR()
-    {
-        // Create OpenGL ES view:
-        int depthSize = 16;
-        int stencilSize = 0;
-        boolean translucent = Vuforia.requiresAlpha();
-
-        mGlView = new SampleApplicationGLView(this);
-        mGlView.init(translucent, depthSize, stencilSize);
-
-        mRenderer = new FrameMarkerRenderer(this, vuforiaAppSession);
-        //mRenderer.setTextures(mTextures);
-        mGlView.setRenderer(mRenderer);
-
-    }
-
-    private void startLoadingAnimation()
-    {
-        LayoutInflater inflater = LayoutInflater.from(this);
-        mUILayout = (RelativeLayout) inflater.inflate(R.layout.camera_overlay,
-                null, false);
-
-        mUILayout.setVisibility(View.VISIBLE);
-        mUILayout.setBackgroundColor(Color.BLACK);
-
-
-        // Adds the inflated layout to the view
-        addContentView(mUILayout, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT));
-    }
 
     @Override
     public void onInitARDone(SampleApplicationException e) {
-        rl.removeAllViews();
-        initApplicationAR();
-        addContentView(mGlView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT));
-        mUILayout.bringToFront();
-        mUILayout.setBackgroundColor(Color.TRANSPARENT);
+        mRenderer.mIsActive = true;
         try {
             vuforiaAppSession.startAR(CameraDevice.CAMERA.CAMERA_DEFAULT);
         } catch (SampleApplicationException e1) {
@@ -160,6 +121,5 @@ public class OpenGL_Renderer extends ActionBarActivity implements SampleApplicat
 
     @Override
     public void onQCARUpdate(State state) {
-
     }
 }
