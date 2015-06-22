39b6789b7de3225084f44a464cb033e8f53ecca2
Fabian Baier <fabian.baier@gmx.de>
2015-06-22 17:54:00 +0200

    datt geht!!! es trackt!

--- app/src/main/java/eu/tobby/momentanpol/MomentanpolGLRenderer.java
+++ app/src/main/java/eu/tobby/momentanpol/MomentanpolGLRenderer.java
@@ -1,8 +1,32 @@
 package eu.tobby.momentanpol;
 
+import java.nio.Buffer;
+import java.util.Vector;
+
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.util.Log;
+
+import com.qualcomm.vuforia.CameraCalibration;
+import com.qualcomm.vuforia.CameraDevice;
+import com.qualcomm.vuforia.Matrix44F;
 import com.qualcomm.vuforia.Renderer;
 import com.qualcomm.vuforia.Vuforia;
+import com.qualcomm.vuforia.Marker;
+import com.qualcomm.vuforia.MarkerResult;
+import com.qualcomm.vuforia.MarkerTracker;
+import com.qualcomm.vuforia.State;
+import com.qualcomm.vuforia.Tool;
+import com.qualcomm.vuforia.TrackableResult;
+
+import eu.tobby.momentanpol.objects.AObject;
+import eu.tobby.momentanpol.objects.CObject;
+import eu.tobby.momentanpol.objects.QObject;
+import eu.tobby.momentanpol.objects.RObject;
+import eu.tobby.momentanpol.utils.Texture;
+import eu.tobby.momentanpol.utils.SampleUtils;
+import eu.tobby.momentanpol.utils.CubeShaders;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -11,23 +35,206 @@ import javax.microedition.khronos.opengles.GL10;
 /**
  * Created by tobby on 11.06.15.
  */
-public class MomentanpolGLRenderer implements GLSurfaceView.Renderer {
+public class MomentanpolGLRenderer implements GLSurfaceView.Renderer
+{
+//private Vector<Texture> mTextures;
+    private Vector<Texture> mTextures;
+// OpenGL ES 2.0 specific:
+    private int shaderProgramID = 0;
+    private int vertexHandle = 0;
+    private int normalHandle = 0;
+    private int textureCoordHandle = 0;
+    private int mvpMatrixHandle = 0;
+    private int texSampler2DHandle = 0;
+    private Matrix44F mProjectionMatrix;
 
-    public MomentanpolGLRenderer() {}
 
-    public void onSurfaceCreated(GL10 gl, EGLConfig config) {}
+    // Constants:
+    static private float kLetterScale = 25.0f;
+    static private float kLetterTranslate = 25.0f;
+
+    private QObject qObject = new QObject();
+    private CObject cObject = new CObject();
+    private AObject aObject = new AObject();
+    private RObject rObject = new RObject();
+
+    public MomentanpolGLRenderer(){
+        //mTextures = new Vector<Texture>();
+    }
+
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        initRendering();
+        Vuforia.onSurfaceCreated();
+    }
 
     public void onDrawFrame(GL10 gl) {
+
+        renderFrame();
+    }
+
+    private void renderFrame() {
+
+        // Clear color and depth buffer
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+        // Get the state from Vuforia and mark the beginning of a rendering
+        // section
+        State state = Renderer.getInstance().begin();
+
         // Get the state from Vuforia and mark the beginning of a rendering section
-        Renderer.getInstance().begin();
+
         // Explicitly render the Video Background
         Renderer.getInstance().drawVideoBackground();
+        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+        GLES20.glEnable(GLES20.GL_CULL_FACE);
+        GLES20.glCullFace(GLES20.GL_BACK);
+
+        for(int trackingIndex = 0; trackingIndex < state.getNumTrackableResults(); trackingIndex++) {
+            TrackableResult trackableResult = state.getTrackableResult(trackingIndex);
+
+            float[] modelViewMatrix = Tool.convertPose2GLMatrix(trackableResult.getPose()).getData();
+            MarkerResult markerResult = (MarkerResult) (trackableResult);
+            Marker marker = (Marker) markerResult.getTrackable();
+            int textureIndex = marker.getMarkerId();
+            Texture thisTexture = mTextures.get(textureIndex);
+
+            Buffer vertices = null;
+            Buffer normals = null;
+            Buffer indices = null;
+            Buffer texCoords = null;
+            int numIndices = 0;
+
+            switch (marker.getMarkerId()) {
+                case 0:
+                    vertices = qObject.getVertices();
+                    normals = qObject.getNormals();
+                    indices = qObject.getIndices();
+                    texCoords = qObject.getTexCoords();
+                    numIndices = qObject.getNumObjectIndex();
+                    break;
+                case 1:
+                    vertices = cObject.getVertices();
+                    normals = cObject.getNormals();
+                    indices = cObject.getIndices();
+                    texCoords = cObject.getTexCoords();
+                    numIndices = cObject.getNumObjectIndex();
+                    break;
+                case 2:
+                    vertices = aObject.getVertices();
+                    normals = aObject.getNormals();
+                    indices = aObject.getIndices();
+                    texCoords = aObject.getTexCoords();
+                    numIndices = aObject.getNumObjectIndex();
+                    break;
+                default:
+                    vertices = rObject.getVertices();
+                    normals = rObject.getNormals();
+                    indices = rObject.getIndices();
+                    texCoords = rObject.getTexCoords();
+                    numIndices = rObject.getNumObjectIndex();
+                    break;
+            }
+            float[] modelViewProjection = new float[16];
+
+
+            Matrix.translateM(modelViewMatrix, 0, -kLetterTranslate,
+                    -kLetterTranslate, 0.f);
+            Matrix.scaleM(modelViewMatrix, 0, kLetterScale, kLetterScale,
+                    kLetterScale);
+            Matrix.multiplyMM(modelViewProjection, 0, getProjectionMatrix().getData(), 0, modelViewMatrix, 0);
+
+            GLES20.glUseProgram(shaderProgramID);
+
+            GLES20.glVertexAttribPointer(vertexHandle, 3, GLES20.GL_FLOAT,
+                    false, 0, vertices);
+            GLES20.glVertexAttribPointer(normalHandle, 3, GLES20.GL_FLOAT,
+                    false, 0, normals);
+            GLES20.glVertexAttribPointer(textureCoordHandle, 2,
+                    GLES20.GL_FLOAT, false, 0, texCoords);
+
+            GLES20.glEnableVertexAttribArray(vertexHandle);
+            GLES20.glEnableVertexAttribArray(normalHandle);
+            GLES20.glEnableVertexAttribArray(textureCoordHandle);
+
+            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D,
+                    thisTexture.mTextureID[0]);
+            GLES20.glUniformMatrix4fv(mvpMatrixHandle, 1, false,
+                    modelViewProjection, 0);
+            GLES20.glUniform1i(texSampler2DHandle, 0);
+            GLES20.glDrawElements(GLES20.GL_TRIANGLES, numIndices,
+                    GLES20.GL_UNSIGNED_SHORT, indices);
+
+            GLES20.glDisableVertexAttribArray(vertexHandle);
+            GLES20.glDisableVertexAttribArray(normalHandle);
+            GLES20.glDisableVertexAttribArray(textureCoordHandle);
+
+        }
+
+        //GLES20.glDisable(GLES20.GL_DEPTH_TEST);
+
         Renderer.getInstance().end();
+
     }
+
 
     // Called when the surface changed size.
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         // Call Vuforia function to handle render surface size changes:
         Vuforia.onSurfaceChanged(width, height);
+    }
+
+    public void setProjectionMatrix()
+    {
+        CameraCalibration camCal = CameraDevice.getInstance().getCameraCalibration();
+        mProjectionMatrix = Tool.getProjectionGL(camCal, 10.0f, 5000.0f);
+    }
+
+    public Matrix44F getProjectionMatrix()
+    {
+        return mProjectionMatrix;
+    }
+
+
+    private void initRendering() {
+
+        // Define clear color
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, Vuforia.requiresAlpha() ? 0.0f
+                : 1.0f);
+        // Now generate the OpenGL texture objects and add settings
+        for (Texture t : mTextures)
+        {
+            GLES20.glGenTextures(1, t.mTextureID, 0);
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, t.mTextureID[0]);
+            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                    GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                    GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+            GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA,
+                    t.mWidth, t.mHeight, 0, GLES20.GL_RGBA,
+                    GLES20.GL_UNSIGNED_BYTE, t.mData);
+        }
+
+        shaderProgramID = SampleUtils.createProgramFromShaderSrc(
+                CubeShaders.CUBE_MESH_VERTEX_SHADER,
+                CubeShaders.CUBE_MESH_FRAGMENT_SHADER);
+
+        vertexHandle = GLES20.glGetAttribLocation(shaderProgramID,
+                "vertexPosition");
+        normalHandle = GLES20.glGetAttribLocation(shaderProgramID,
+                "vertexNormal");
+        textureCoordHandle = GLES20.glGetAttribLocation(shaderProgramID,
+                "vertexTexCoord");
+        mvpMatrixHandle = GLES20.glGetUniformLocation(shaderProgramID,
+                "modelViewProjectionMatrix");
+        texSampler2DHandle = GLES20.glGetUniformLocation(shaderProgramID,
+                "texSampler2D");
+
+    }
+
+    public void setTextures(Vector<Texture> textures)
+    {
+        mTextures = textures;
+
     }
 }
--- app/src/main/java/eu/tobby/momentanpol/OpenGL_Renderer.java
+++ app/src/main/java/eu/tobby/momentanpol/OpenGL_Renderer.java
@@ -2,9 +2,11 @@ package eu.tobby.momentanpol;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 /*import android.view.Menu;
 import android.view.MenuItem;*/
@@ -12,38 +14,66 @@ import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.view.WindowManager;
 
+import com.qualcomm.vuforia.CameraCalibration;
 import com.qualcomm.vuforia.CameraDevice;
+import com.qualcomm.vuforia.Marker;
+import com.qualcomm.vuforia.MarkerTracker;
+import com.qualcomm.vuforia.Matrix44F;
 import com.qualcomm.vuforia.Renderer;
 import com.qualcomm.vuforia.State;
+import com.qualcomm.vuforia.Tool;
+import com.qualcomm.vuforia.Tracker;
+import com.qualcomm.vuforia.TrackerManager;
+import com.qualcomm.vuforia.Vec2F;
 import com.qualcomm.vuforia.Vec2I;
 import com.qualcomm.vuforia.VideoBackgroundConfig;
 import com.qualcomm.vuforia.VideoMode;
 import com.qualcomm.vuforia.Vuforia;
 import com.qualcomm.vuforia.Vuforia.UpdateCallbackInterface;
 
+import java.util.Vector;
+import java.util.concurrent.locks.Lock;
+
+import eu.tobby.momentanpol.utils.Texture;
+
 
 public class OpenGL_Renderer extends ActionBarActivity implements UpdateCallbackInterface {
+
+    private static final String LOGTAG = "OpenGL_renderer";
 
     private View androidView;
     private MomentanpolGLRenderer mRenderer;
     GLSurfaceView glSurfaceView;
+    private Marker dataSet[];
+    private Vector<Texture> mTextures;
+    private Object mShutdownLock = new Object();
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Create Vuforia instance, initialize it and start the camera
-        initAR();
+        mTextures = new Vector<>();
+        loadTextures();
+
         // Create object of an OpenGL-Viewer with OpenGL2.0
         glSurfaceView = new GLSurfaceView(this);
         glSurfaceView.setEGLContextClientVersion(2);
         // Create Renderer for OpenGL, add it to the view and show it
         mRenderer = new MomentanpolGLRenderer();
+        mRenderer.setTextures(mTextures);
         glSurfaceView.setRenderer(mRenderer);
         setContentView(glSurfaceView);
+        initAR();
         // Make a view out of the Designer-XML and add this view on top of the OpenGL-Viewer
         LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         androidView = inflater.inflate(R.layout.activity_open_gl__renderer, null);
         addContentView(androidView, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+
+
+
+
     }
 
     @Override
@@ -82,11 +112,49 @@ public class OpenGL_Renderer extends ActionBarActivity implements UpdateCallback
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         Vuforia.setInitParameters(this, Vuforia.GL_20, "AcCmXLj/////AAAAAWn4W4WOD0/up7Ehu28I5VBQq6sv1WL7JaOAAvhmS98exI+JmpBsPdHy4GnaLxhmgOq3BSpWKFZR4eh6xL2K2NIGN4kPDW8fQwwhKv+7uusQoE5Grc/DOTM0NMZ/G/UPJQC59Uj/SnYvr67zfibax4kVrv+tNzLkcqn+pvhLcdFX1HATddnCCb9IwC2QEc+qX2HSLwxDlS/87FVlhcsUB/NeICVSVTtB5+buqEwOGy+4ZLwJjW5RFrGWX9SLMWHffO9K7X4mn2JQqRt8ZBJXMbixO54BFT+wA7JAhtfznUN33z3QBiE8Uce3aCI8Fh6gBVUt6b35sv8IMmnbZGJ9iA6XurRBoNwJEm7bve2myxu/");
         while(Vuforia.init()<100){}
+        boolean initTrackersResult;
+        initTrackersResult = doInitTrackers();
+        if(initTrackersResult) {
+            doLoadTrackersData();
+        }
         CameraDevice.getInstance().init(CameraDevice.CAMERA.CAMERA_DEFAULT);
         configureVideoBackground();
         CameraDevice.getInstance().selectVideoMode(CameraDevice.MODE.MODE_DEFAULT);
         CameraDevice.getInstance().start();
         CameraDevice.getInstance().setFocusMode(CameraDevice.FOCUS_MODE.FOCUS_MODE_CONTINUOUSAUTO);
+        mRenderer.setProjectionMatrix();
+
+    }
+
+    public boolean doInitTrackers() {
+        boolean result = true;
+        TrackerManager trackerManager = TrackerManager.getInstance();
+        Tracker trackerBase = trackerManager.initTracker(MarkerTracker.getClassType());
+        MarkerTracker markerTracker = (MarkerTracker) (trackerBase);
+        if(markerTracker == null) {
+            result = false;
+            Log.e(LOGTAG, "MarkerTracker not initialized");
+        }
+        return result;
+    }
+
+    public boolean doLoadTrackersData(){
+
+        TrackerManager tManager = TrackerManager.getInstance();
+        MarkerTracker markerTracker = (MarkerTracker) tManager.getTracker(MarkerTracker.getClassType());
+        if (markerTracker == null)
+                return false;
+        dataSet = new Marker[4];
+        dataSet[0] = markerTracker.createFrameMarker(0, "MarkerQ", new Vec2F(50, 50));
+        dataSet[1] = markerTracker.createFrameMarker(1, "MarkerC", new Vec2F(50, 50));
+        dataSet[2] = markerTracker.createFrameMarker(2, "MarkerA", new Vec2F(50, 50));
+        dataSet[3] = markerTracker.createFrameMarker(3, "MarkerR", new Vec2F(50, 50));
+        markerTracker.start();
+        return true;
+        // Here Exception handling
+
+
+
     }
 
     private void configureVideoBackground() {
@@ -114,4 +182,19 @@ public class OpenGL_Renderer extends ActionBarActivity implements UpdateCallback
     // Callback called every cycle
     @Override
     public void QCAR_onUpdate(State s) {}
+
+    private void loadTextures()
+    {
+        mTextures.add(Texture.loadTextureFromApk("FrameMarkers/letter_Q.png",
+                getAssets()));
+        mTextures.add(Texture.loadTextureFromApk("FrameMarkers/letter_C.png",
+                getAssets()));
+        mTextures.add(Texture.loadTextureFromApk("FrameMarkers/letter_A.png",
+                getAssets()));
+        mTextures.add(Texture.loadTextureFromApk("FrameMarkers/letter_R.png",
+                getAssets()));
+    }
+
+
+
 }
