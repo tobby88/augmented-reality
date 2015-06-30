4bc28d74422786134859ee6d490db5b3e42e77bf
Tobias Holst <tobby@tobby.eu>
2015-06-30 16:47:41 +0200

    Add Bart

--- app/src/main/java/eu/tobby/momentanpol/Main_Activity.java
+++ app/src/main/java/eu/tobby/momentanpol/Main_Activity.java
@@ -114,12 +114,13 @@ public class Main_Activity extends ActionBarActivity implements UpdateCallbackIn
         MarkerTracker markerTracker = (MarkerTracker) tManager.getTracker(MarkerTracker.getClassType());
         if (markerTracker == null)
                 return false;
-        dataSet = new Marker[5];
+        dataSet = new Marker[6];
         dataSet[0] = markerTracker.createFrameMarker(0, "MarkerQ", new Vec2F(50, 50));
         dataSet[1] = markerTracker.createFrameMarker(1, "MarkerC", new Vec2F(50, 50));
         dataSet[2] = markerTracker.createFrameMarker(2, "MarkerA", new Vec2F(50, 50));
         dataSet[3] = markerTracker.createFrameMarker(3, "MarkerR", new Vec2F(50, 50));
         dataSet[4] = markerTracker.createFrameMarker(4, "Momentanpol", new Vec2F(50, 50));
+        dataSet[5] = markerTracker.createFrameMarker(5, "Bart", new Vec2F(50, 50));
         markerTracker.start();
         return true;
         // Here Exception handling
